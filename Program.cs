da863aa45ed36d552246b360327d4ed91fdfaa04
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:08:20 -0400

    Agregando el proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+            int cantLados;
+            float mediaLados, perimetro;
+
+            Console.Write("Cantidad de lados de la figura: ");
+            cantLados = Int32.Parse(Console.ReadLine());
+            Console.Write("\nMedia de un lado: ");
+            mediaLados = Int32.Parse(Console.ReadLine());
+
+            perimetro = cantLados * mediaLados;
+
+            Console.WriteLine("El perimetro es: {0}", perimetro);
